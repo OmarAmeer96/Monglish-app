45eaf34df6373efe7014f387c1e6511a7f5f1907
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-13 01:02:03 +0200

    Implement user login check and add SimpleBlocObserver for better state management

---
+++ lib/core/helpers/check_if_user_is_logged_in.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:monglish_app/core/helpers/constants.dart';
+import 'package:monglish_app/core/helpers/extensions.dart';
+import 'package:monglish_app/core/helpers/shared_pref_helper.dart';
+
+Future<void> checkIfUserIsLoggedIn() async {
+  String? userToken =
+      await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
+  debugPrint('User Token: $userToken');
+  if (userToken.isNullOrEmpty()) {
+    isUserLoggedIn = false;
+  } else {
+    isUserLoggedIn = true;
+  }
+  debugPrint('Is User Logged In: $isUserLoggedIn');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:monglish_app/core/di/dependency_injection.dart';
-import 'package:monglish_app/core/helpers/constants.dart';
-import 'package:monglish_app/core/helpers/extensions.dart';
-import 'package:monglish_app/core/helpers/shared_pref_helper.dart';
+import 'package:monglish_app/core/helpers/check_if_user_is_logged_in.dart';
+import 'package:monglish_app/core/helpers/simple_bloc_observer.dart';
 import 'package:monglish_app/core/routing/app_router.dart';
 import 'package:monglish_app/moonglish_app.dart';
 
@@ -12,25 +12,15 @@ void main() async {
 
   setupGetIt();
 
+  Bloc.observer = SimpleBlocObserver();
+
   await ScreenUtil.ensureScreenSize();
 
-  // await checkIfUserIsLoggedIn();
+  await checkIfUserIsLoggedIn();
 
   runApp(
     MonglishApp(
       appRouter: AppRouter(),
     ),
   );
-}
-
-Future<void> checkIfUserIsLoggedIn() async {
-  String? userToken =
-      await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
-  debugPrint('User Token: $userToken');
-  if (userToken.isNullOrEmpty()) {
-    isUserLoggedIn = false;
-  } else {
-    isUserLoggedIn = true;
-  }
-  debugPrint('Is User Logged In: $isUserLoggedIn');
 }
