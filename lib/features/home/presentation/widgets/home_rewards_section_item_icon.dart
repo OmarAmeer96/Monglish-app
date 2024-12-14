300a452d9ad68307ac1be71813c999f3ad2c3928
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-14 22:11:02 +0200

    Enhance HomeRewardsSectionItemIcon with notification badge and improved padding

--- lib/features/home/presentation/widgets/home_rewards_section_item_icon.dart
+++ lib/features/home/presentation/widgets/home_rewards_section_item_icon.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:monglish_app/core/theming/styles.dart';
 
 class HomeRewardsSectionItemIcon extends StatelessWidget {
   const HomeRewardsSectionItemIcon({
@@ -15,21 +16,52 @@ class HomeRewardsSectionItemIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(3),
-      width: 55,
-      height: 55,
-      decoration: ShapeDecoration(
-        color: const Color(0xFFB68E2C),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(6),
+          width: 55,
+          height: 55,
+          decoration: ShapeDecoration(
+            color: const Color(0xFFB68E2C),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          child: Center(
+            child: SvgPicture.asset(
+              icon,
+            ),
+          ),
         ),
-      ),
-      child: Center(
-        child: SvgPicture.asset(
-          icon,
+        Positioned(
+          top: -10,
+          right: -10,
+          child: Container(
+            width: 20,
+            height: 20,
+            decoration: const ShapeDecoration(
+              color: Color(0xFF08C552),
+              shape: OvalBorder(
+                side: BorderSide(
+                  width: 1,
+                  strokeAlign: BorderSide.strokeAlignOutside,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                count,
+                style: Styles.font14BlackMedium.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
