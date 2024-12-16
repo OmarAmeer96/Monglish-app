265a453d1e3e3ed081e05352c5cf96538d3f4c09
OmarAmeer96 <omar.ameer244@gmail.com>
2024-12-16 15:15:37 +0200

    Implented the search bar type 2

--- lib/features/home/presentation/widgets/home_view_app_bar.dart
+++ lib/features/home/presentation/widgets/home_view_app_bar.dart
@@ -10,40 +10,13 @@ class HomeViewAppBar extends StatefulWidget {
   _HomeViewAppBarState createState() => _HomeViewAppBarState();
 }
 
-class _HomeViewAppBarState extends State<HomeViewAppBar>
-    with SingleTickerProviderStateMixin {
+class _HomeViewAppBarState extends State<HomeViewAppBar> {
   bool _isSearchOpen = false;
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
-  }
 
   void _toggleSearch() {
     setState(() {
       _isSearchOpen = !_isSearchOpen;
-      if (_isSearchOpen) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
     });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
@@ -65,31 +38,36 @@ class _HomeViewAppBarState extends State<HomeViewAppBar>
                   'assets/svgs/notifications_icon.svg',
                 ),
               ),
+              // Animate the width of the search bar
               AnimatedContainer(
                 duration: const Duration(milliseconds: 300),
-                width: _isSearchOpen
-                    ? MediaQuery.of(context).size.width * 0.5
-                    : 0.0,
                 curve: Curves.easeInOut,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search...',
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.close),
+                width: _isSearchOpen
+                    ? MediaQuery.of(context).size.width * 0.55
+                    : 0,
+                child: _isSearchOpen
+                    ? TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search...',
+                          suffixIcon: IconButton(
+                            icon: const Icon(Icons.close),
+                            onPressed: _toggleSearch,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+              ),
+              !_isSearchOpen
+                  ? IconButton(
                       onPressed: _toggleSearch,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                ),
-              ),
-              IconButton(
-                onPressed: _toggleSearch,
-                icon: SvgPicture.asset(
-                  'assets/svgs/search_icon.svg',
-                ),
-              ),
+                      icon: SvgPicture.asset(
+                        'assets/svgs/search_icon.svg',
+                      ),
+                    )
+                  : const SizedBox.shrink(),
               const Spacer(),
               const HomeProfilePicture(),
             ],
